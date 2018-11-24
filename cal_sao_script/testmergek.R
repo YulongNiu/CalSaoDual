e8cfb7a743f42d1ce108f0a125dec1df6a495156
Yulong Niu <yulong.niu@hotmail.com>
2018-11-24 18:08:57 +0800

    complete SplitSpe func

--- cal_sao_script/testmergek.R
+++ cal_sao_script/testmergek.R
@@ -13,7 +13,7 @@ saonum <- readBStringSet('sao_cdna_name.fa') %>%
 
 mixec <- read.table('testpseudo/pseudoalignments.ec', stringsAsFactors = FALSE) %>%
   `[`(, 2) %>%
-  RNASeqQuant:::SplitEC
+  RNASeqQuant:::SplitEC(.)
 
 ## cal number: 6226
 ## sao number: 2844
@@ -28,5 +28,50 @@ hasMixEc <- sapply(mixec, function(x) {
 sum(hasMixEc) == length(mixec)
 
 ## no mix ec in cal and sao
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~split ec/efflen/ecount~~~~~~~~~~~~~~~~
+mixec <- read.table('testpseudo/pseudoalignments.ec', stringsAsFactors = FALSE) %>%
+  `[`(, 2)
+
+mixeccount <- read.table('testpseudo/pseudoalignments.tsv', stringsAsFactors = FALSE) %>%
+  `[`(, 2)
+
+mixefflen <- read.table('testquant/abundance.tsv', stringsAsFactors = FALSE, header = TRUE) %>%
+  `[`(, 3)
+
+SplitSpe <- function(ec, eccount, efflen, spenum) {
+
+  require('RNASeqQuant')
+  require('magrittr')
+
+  ## ec of the species must be completely separated
+  ## two species
+
+  numec <- RNASeqQuant:::SplitEC(ec)
+  logic1st <- sapply(numec, function(x) {
+    lenx <- length(x)
+    eachLogic <- sum(x < spenum[1]) == lenx
+    return(eachLogic)
+  })
+
+  res <- vector('list', 2)
+  res[[1]]$ec <- numec[logic1st]
+  res[[1]]$ec %<>% sapply(., paste, collapse = ',')
+  res[[1]]$count <- eccount[logic1st]
+  res[[1]]$efflen <- efflen[1:(spenum[1])]
+
+  res[[2]]$ec <- numec[!logic1st]
+  res[[2]]$ec %<>% sapply(., function(x) {
+    return(paste(x - spenum[1], collapse = ','))
+  })
+  res[[2]]$count <- eccount[!logic1st]
+  res[[2]]$efflen <- efflen[(spenum[1] + 1) : (spenum[1] + spenum[2])]
+
+  return(res)
+}
+
+tmp1 <- SplitSpe(mixec, mixeccount, mixefflen, c(calnum, saonum))
+
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ################################################################
