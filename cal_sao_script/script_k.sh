bf7028d1e14e2d431ff6ecc009b44dae358e2424
Yulong Niu <yulong.niu@hotmail.com>
2018-11-24 16:58:53 +0800

    add k quant for all samples

--- cal_sao_script/script_k.sh
+++ cal_sao_script/script_k.sh
@@ -5,7 +5,6 @@ KALLISTO_PATH=/home/Yulong/Biotools/kallisto_linux-v0.44.0
 PROJECT_PATH=/extDisk2/cal_sao
 
 INDEX_PATH=${PROJECT_PATH}/kallisto_results
-
 CLEAN_PATH=${PROJECT_PATH}/cleandata
 
 ## merge fa
@@ -16,31 +15,67 @@ CLEAN_PATH=${PROJECT_PATH}/cleandata
 #                 -i ${INDEX_PATH}/cal_sao_idx \
 #                 ${INDEX_PATH}/cal_sao_cdna_name.fa
 
-cd ${INDEX_PATH}
+# ${KALLISTO_PATH}/kallisto index \
+#                 -i ${INDEX_PATH}/cal_idx \
+#                 ${INDEX_PATH}/cal_cdna_name.fa
 
-## pseudoquant
-${KALLISTO_PATH}/kallisto quant \
-                -i cal_sao_idx \
-                -t 8 \
-                -o testpseudo \
-                -l \
-                cal_sao_R1.fq.gz cal_sao_R2.fq.gz
+# ${KALLISTO_PATH}/kallisto index \
+#                 -i ${INDEX_PATH}/sao_idx \
+#                 ${INDEX_PATH}/sao_cdna_name.fa
 
 
-# ## quantification
-# cd ${CLEAN_PATH}
+cd ${CLEAN_PATH}
+
+# ## pseudoquant
+# ${KALLISTO_PATH}/kallisto quant \
+#                 -i ${INDEX_PATH}/cal_sao_idx \
+#                 -t 8 \
+#                 -o testpseudo \
+#                 Sa_CAF2_1_1_R1.fq.gz Sa_CAF2_1_1_R2.fq.gz
 
 
-# fmember=('4h_sm_1' '4h_sm_2' '4h_sm_3' '4h_srta_1' '4h_srta_2' '4h_srta_3' '24h_sm_1' '24h_sm_2' '24h_sm_3' '24h_srta_1' '24h_srta_2' '24h_srta_3')
-
-# for i in "${fmember[@]}"
+## quantification
+## cal
+# callabel=('CAF2_1_1' 'CAF2_1_2' 'CAF2_1_3')
+# for i in "${callabel[@]}"
 # do
 #     echo "Quantify ${i}"
 #     ${KALLISTO_PATH}/kallisto quant \
 #                     -t 8 \
-#                     -i ${INDEX_PATH}/smu_kallisto_idx \
-#                     -o ${RES_PATH}/${i} \
+#                     -i ${INDEX_PATH}/cal_idx \
+#                     -o ${INDEX_PATH}/${i} \
 #                     ${i}_R1.fq.gz ${i}_R2.fq.gz
 # done
+
+# ## sao
+# saolabel=('Sa_1' 'Sa_2' 'Sa_3')
+# for i in "${saolabel[@]}"
+# do
+#     echo "Quantify ${i}"
+#     ${KALLISTO_PATH}/kallisto quant \
+#                     -t 8 \
+#                     -i ${INDEX_PATH}/sao_idx \
+#                     -o ${INDEX_PATH}/${i} \
+#                     ${i}_R1.fq.gz ${i}_R2.fq.gz
+# done
+
+## cal_sao
+calsaolabel=('Sa_CAF2_1_1' 'Sa_CAF2_1_2' 'Sa_CAF2_1_3')
+for i in "${calsaolabel[@]}"
+do
+    echo "Quantify ${i}"
+    ${KALLISTO_PATH}/kallisto quant \
+                    -t 8 \
+                    -i ${INDEX_PATH}/cal_sao_idx \
+                    -o ${INDEX_PATH}/${i} \
+                    ${i}_R1.fq.gz ${i}_R2.fq.gz
+
+    ## pseudoquant
+    echo "Pseudo ${i}"
+    ${KALLISTO_PATH}/kallisto pseudo \
+                        -i ${INDEX_PATH}/cal_sao_idx \
+                        -o ${INDEX_PATH}/${i}_pseudo \
+                        ${i}_R1.fq.gz ${i}_R2.fq.gz
+done
 
 date
